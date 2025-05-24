f463389c4baab1729a520e4e7cc8b05cf2716fe5
Saiprabha06 <saiprabha.v@rediffmail.com>
2025-05-24 15:49:11 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,33 @@
+'''
+Task 1: Perform Basic Mathematical Operations
+
+# Addition
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+total = (num1 + num2)
+print('Addition: ', total)
+
+# Subtraction
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number:'))
+sub = (num1 - num2)
+print('Subtraction: ', sub)
+
+# Multiplication
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+mul = (num1 * num2)
+print('Multiplication: ', mul)
+
+# Division
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+div = (num1 / num2)
+print('Division: ', div)
+'''
+
+# Task 2: Create a Personalized Greeting
+name1 = input('Enter your first name: ')
+name2 = input('Enter your last name: ')
+full_name = (name1 + ' ' + name2)
+print('Hello, ' + full_name + '!' ' Welcome to Python Program.')
